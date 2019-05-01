78c56f84a21cbc862d3e2e91cf75cdd8a6bd2bea
Matteo Crippa <matteo@boostco.de>
2019-05-01 19:27:43 +0200

    fix naming

--- chooseWallpaper.sh
+++ chooseWallpaper.sh
@@ -2,54 +2,23 @@
 
 case $(date +%_H | sed 's/ //') in
     0|1|2|3|4)
-        feh --bg-fill ~/Pictures/Wallpapers/mojave_dynamic_16.jpeg
+        feh --bg-fill ~/.spells/wallpaper/01.jpg
         ;;
-    5)
-        feh --bg-fill ~/Pictures/Wallpapers/mojave_dynamic_1.jpeg
+    5|6|7|8|9)
+        feh --bg-fill ~/.spells/wallpaper/02.jpg
         ;;
-    6)
-        feh --bg-fill ~/Pictures/Wallpapers/mojave_dynamic_2.jpeg
+    10|11|12|13|14)
+        feh --bg-fill ~/.spells/wallpaper/03.jpg
         ;;
-    7)
-        feh --bg-fill ~/Pictures/Wallpapers/mojave_dynamic_3.jpeg
+    15|16|17|18|19)
+        feh --bg-fill ~/.spells/wallpaper/04.jpg
         ;;
-    8)
-        feh --bg-fill ~/Pictures/Wallpapers/mojave_dynamic_4.jpeg
+    20|21)
+        feh --bg-fill ~/.spells/wallpaper/05.jpg
         ;;
-    9)
-        feh --bg-fill ~/Pictures/Wallpapers/mojave_dynamic_5.jpeg
-        ;;
-    10)
-        feh --bg-fill ~/Pictures/Wallpapers/mojave_dynamic_6.jpeg
-        ;;
-    11|12)
-        feh --bg-fill ~/Pictures/Wallpapers/mojave_dynamic_7.jpeg
-        ;;
-    13|14)
-        feh --bg-fill ~/Pictures/Wallpapers/mojave_dynamic_8.jpeg
-        ;;
-    15)
-        feh --bg-fill ~/Pictures/Wallpapers/mojave_dynamic_9.jpeg
-        ;;
-    16|17)
-        feh --bg-fill ~/Pictures/Wallpapers/mojave_dynamic_10.jpeg
-        ;;
-    18)
-        feh --bg-fill ~/Pictures/Wallpapers/mojave_dynamic_11.jpeg
-        ;;
-    19)
-        feh --bg-fill ~/Pictures/Wallpapers/mojave_dynamic_12.jpeg
-        ;;
-    20)
-        feh --bg-fill ~/Pictures/Wallpapers/mojave_dynamic_13.jpeg
-        ;;
-    21|22)
-        feh --bg-fill ~/Pictures/Wallpapers/mojave_dynamic_14.jpeg
-        ;;
-    23)
-        feh --bg-fill ~/Pictures/Wallpapers/mojave_dynamic_15.jpeg
+    22|23)
+        feh --bg-fill ~/.spells/wallpaper/06.jpg
         ;;
     *)
         ;;
 esac
-
