36e08021162831f6b5602df72dc575403a6351c5
Matteo Crippa <matteo@boostco.de>
2019-05-02 17:55:51 +0200

    fix

--- naturalScrollOn.sh
+++ naturalScrollOn.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-device=$(xinput list | grep TouchP  ad | grep -Eo '[0-9]{1,9}' | sed -n 2p)
+device=$(xinput list | grep TouchPad | grep -Eo '[0-9]{1,9}' | sed -n 2p)
 
 prop=$(xinput list-props "$device" | grep "Natural Scrolling Enabled" | grep -oP '\(\K[^\])]+' | sed -n 1p)
 
