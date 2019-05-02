f4ccd03b513e01545595bd0fcdbf861d6ec3fce2
Matteo Crippa <matteo@boostco.de>
2019-05-02 14:10:00 +0200

    test

--- autolock.sh
+++ autolock.sh
@@ -1,5 +1,2 @@
-#!/usr/bin/env sh
-exec xautolock -detectsleep
-  -time 10 -locker betterlockscreen \
-  -notify 30 \
-  -notifier "notify-send -u critical -t 10000 -- 'Locking screen in 30 seconds'"
+#!/bin/bash
+exec xautolock -detectsleep -time 1 -locker betterlockscreen -notify 30 -notifier "notify-send -u critical -t 10000 -- 'Locking screen in 30 seconds'"
