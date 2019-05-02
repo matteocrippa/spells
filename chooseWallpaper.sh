713d8b02718e647a9a19e35beed10c4d0d1d26a0
Matteo Crippa <matteo@boostco.de>
2019-05-02 20:35:24 +0200

    fixed wallpaper

--- chooseWallpaper.sh
+++ chooseWallpaper.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env sh
+feh --bg-fill ~/.spells/wallpaper/07.jpg
 
-case $(date +%_H | sed 's/ //') in
-    0|1|2|3|4)
-        feh --bg-fill ~/.spells/wallpaper/01.jpg
-        ;;
-    5|6|7|8|9)
-        feh --bg-fill ~/.spells/wallpaper/02.jpg
-        ;;
-    10|11|12|13|14)
-        feh --bg-fill ~/.spells/wallpaper/03.jpg
-        ;;
-    15|16|17|18|19)
-        feh --bg-fill ~/.spells/wallpaper/04.jpg
-        ;;
-    20|21)
-        feh --bg-fill ~/.spells/wallpaper/05.jpg
-        ;;
-    22|23)
-        feh --bg-fill ~/.spells/wallpaper/06.jpg
-        ;;
-    *)
-        ;;
-esac
+#case $(date +%_H | sed 's/ //') in
+#    0|1|2|3|4)
+#        feh --bg-fill ~/.spells/wallpaper/01.jpg
+#        ;;
+#    5|6|7|8|9)
+#        feh --bg-fill ~/.spells/wallpaper/02.jpg
+#        ;;
+#    10|11|12|13|14)
+#        feh --bg-fill ~/.spells/wallpaper/03.jpg
+#        ;;
+#    15|16|17|18|19)
+#        feh --bg-fill ~/.spells/wallpaper/04.jpg
+#        ;;
+#    20|21)
+#        feh --bg-fill ~/.spells/wallpaper/05.jpg
+#        ;;
+#    22|23)
+#        feh --bg-fill ~/.spells/wallpaper/06.jpg
+#        ;;
+#    *)
+#        ;;
+#esac
