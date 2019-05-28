7cc0e2e83022495df86ced2bfa7cfef580f392bb
Matteo Crippa <matteo.crippa+dev@gmail.com>
2019-05-28 14:35:31 +0200

    fix battery script

--- polybar/battery-combined-udev.sh
+++ polybar/battery-combined-udev.sh
@@ -1,88 +1,113 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-battery_print() {
-    path_ac="/sys/class/power_supply/AC"
-    path_battery_0="/sys/class/power_supply/BAT0"
-    path_battery_1="/sys/class/power_supply/BAT1"
+# main idea: all that you need to probe battery performance lie in this beautiful file:
+# $ cat /sys/class/power_supply/BAT0/uevent
+# additional helper files: $ ls /sys/class/power_supply/BAT0
+# tip: thinkpads also have BAT1
 
-    ac=0
-    battery_level_0=0
-    battery_level_1=0
-    battery_max_0=0
-    battery_max_1=0
-
-    if [ -f "$path_ac/online" ]; then
-        ac=$(cat "$path_ac/online")
-    fi
-
-    if [ -f "$path_battery_0/energy_now" ]; then
-        battery_level_0=$(cat "$path_battery_0/energy_now")
-    fi
-
-    if [ -f "$path_battery_0/energy_full" ]; then
-        battery_max_0=$(cat "$path_battery_0/energy_full")
-    fi
-
-    if [ -f "$path_battery_1/energy_now" ]; then
-        battery_level_1=$(cat "$path_battery_1/energy_now")
-    fi
-
-    if [ -f "$path_battery_1/energy_full" ]; then
-        battery_max_1=$(cat "$path_battery_1/energy_full")
-    fi
-
-    battery_level=$(("$battery_level_0 + $battery_level_1"))
-    battery_max=$(("$battery_max_0 + $battery_max_1"))
-
-    battery_percent=$(("$battery_level * 100"))
-    battery_percent=$(("$battery_percent / $battery_max"))
-
-    if [ "$ac" -eq 1 ]; then
-        icon=""
-
-        if [ "$battery_percent" -gt 65 ]; then
-            echo "$icon"
-        else
-            echo "$icon $battery_percent %"
-        fi
-    else
-        if [ "$battery_percent" -gt 80 ]; then
-            icon=""
-        elif [ "$battery_percent" -gt 65 ]; then
-            icon=""
-        elif [ "$battery_percent" -gt 35 ]; then
-            icon=""
-        elif [ "$battery_percent" -gt 5 ]; then
-            icon=""
-        else
-            icon=""
-        fi
-
-        echo "$icon $battery_percent %"
-    fi
+format_minutes() {
+  if (( $1 < 10 )); then
+    echo "0"$1
+    return 0
+  fi
+  echo $1
 }
 
-path_pid="~/.spells/polybar/battery-combined-udev.pid"
+battery_print() {
+  path_ac="/sys/class/power_supply/AC"
+  path_battery_0="/sys/class/power_supply/BAT0"
+  path_battery_1="/sys/class/power_supply/BAT1"
+
+  ac=0
+  battery_level_0=0
+  battery_level_1=0
+  battery_max_0=0
+  battery_max_1=0
+
+  if [ -f "$path_ac/online" ]; then
+    ac=$(cat "$path_ac/online")
+  fi
+
+  if [ -f "$path_battery_0/energy_now" ]; then
+    battery_level_0=$(cat "$path_battery_0/energy_now")
+  fi
+
+  if [ -f "$path_battery_0/energy_full" ]; then
+    battery_max_0=$(cat "$path_battery_0/energy_full")
+  fi
+
+  if [ -f "$path_battery_1/energy_now" ]; then
+    battery_level_1=$(cat "$path_battery_1/energy_now")
+  fi
+
+  if [ -f "$path_battery_1/energy_full" ]; then
+    battery_max_1=$(cat "$path_battery_1/energy_full")
+  fi
+
+  battery_level=$(("$battery_level_0 + $battery_level_1"))
+  battery_max=$(("$battery_max_0 + $battery_max_1"))
+
+  battery_percent=$(("$battery_level * 100"))
+  battery_percent=$(("$battery_percent / $battery_max"))
+  battery_percent_float=$(bc -l <<< "($battery_level * 100) / $battery_max" | cut -c1-4)
+
+  if [ "$ac" -eq 1 ]; then
+    icon=""
+
+    if [ "$battery_percent" -gt 97 ]; then
+      echo "$icon"
+    else
+      echo "$icon ${battery_percent}%"
+    fi
+  else
+    if [[ $(cat /sys/class/power_supply/BAT0/power_now) = 0 ]]; then
+      power=$(cat /sys/class/power_supply/BAT1/power_now)
+    else
+      power=$(cat /sys/class/power_supply/BAT0/power_now)
+    fi
+    bat0_energy=$(cat /sys/class/power_supply/BAT0/energy_now)
+    bat1_energy=$(cat /sys/class/power_supply/BAT1/energy_now)
+    total_energy=$(bc -l <<< "$bat0_energy + $bat1_energy")
+    hours=$(bc <<< "$total_energy/$power" | cut -c1-4)"hs"
+    hours_discrete=$(bc <<< "$total_energy/$power")
+    hours_float=$(bc -l <<< "$total_energy/$power")
+    minutes=$(echo "scale=0; (($hours_float - $hours_discrete) * 60)/1" | bc)
+    formatted_minutes=$(format_minutes $minutes)
+    power_float=$(bc -l <<< "$power/1000000" | cut -c1-4)
+    remaining="羽"${hours_discrete}:${formatted_minutes}
+
+    if [ "$battery_percent" -gt 85 ]; then
+      icon=""
+
+    elif [ "$battery_percent" -gt 60 ]; then
+      icon=""
+    elif [ "$battery_percent" -gt 35 ]; then
+      icon=""
+    elif [ "$battery_percent" -gt 10 ]; then
+      icon=""
+    else
+      icon=""
+    fi
+    echo "$icon ${battery_percent_float}% ($remaining) [${power_float}W]"
+  fi
+}
 
 case "$1" in
-    --update)
-        pid=$(cat $path_pid)
+  --update)
+    pid="~/.spells/polybar/battery-combined-udev.pid"
+    if [ "$pid" != "" ]; then
+      kill -10 "$pid"
+    fi
+    ;;
+  *)
+    trap exit INT
+    trap "echo" USR1
 
-        if [ "$pid" != "" ]; then
-            kill -10 "$pid"
-        fi
-        ;;
-    *)
-        echo $$ > $path_pid
+    while true; do
+      battery_print
 
-        trap exit INT
-        trap "echo" USR1
-
-        while true; do
-            battery_print
-
-            sleep 30 &
-            wait
-        done
-        ;;
+      sleep 3 &
+      wait
+    done
+    ;;
 esac
